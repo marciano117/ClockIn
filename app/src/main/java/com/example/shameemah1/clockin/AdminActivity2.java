750f482b31d343fc0896e8fb7d845f3014b0e242
marciano117 <petemarciano2@gmail.com>
2016-12-03 13:35:19 -0600

    Got login functionality to work successfully!

---
+++ app/src/main/java/com/example/shameemah1/clockin/AdminActivity2.java
@@ -0,0 +1,13 @@
+package com.example.shameemah1.clockin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AdminActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_activity_2nd);
+    }
+}
