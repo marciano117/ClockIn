a03c179bb33d49630c9f5dcedea44c6cd7793773
marciano117 <petemarciano2@gmail.com>
2016-12-03 12:45:48 -0600

    Added 3 hard coded users and added edittext vars for user and pass fields.

--- app/src/main/java/com/example/shameemah1/clockin/MainActivity.java
+++ app/src/main/java/com/example/shameemah1/clockin/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.shameemah1.clockin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String name1 = "admin";
+    private String name2 = "Peter";
+    private String name3 = "Justin";
+    private String pass1 = "password";
+    private String pass2 = "Marciano";
+    private String pass3 = "Kvedaras";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText loginUsername = (EditText) findViewById(R.id.username);
+        EditText loginPassword = (EditText) findViewById(R.id.password);
+
     }
 }
