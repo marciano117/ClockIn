32343d7f31a14aa99c92ab9e6b41b89063c8d048
Emmanuel Zambrano <ezambran@hawk.iit.edu>
2016-12-03 15:03:20 -0600

    Merge branch 'master' of https://github.com/marciano117/ClockIn
    # Conflicts:
    #	app/src/main/java/com/example/shameemah1/clockin/UserActivity2.java

--- app/src/main/java/com/example/shameemah1/clockin/AdminActivity2.java
+++ app/src/main/java/com/example/shameemah1/clockin/AdminActivity2.java
@@ -1,7 +1,9 @@
 package com.example.shameemah1.clockin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class AdminActivity2 extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class AdminActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_activity_2nd);
+
+
+        //get user information from login page
+        Intent intent = getIntent();
+        String username = intent.getStringExtra(MainActivity.USERNAME);
+        //TextView usernameTitle = (TextView) findViewById(R.id.username);
+        //usernameTitle.setText(username);
+
     }
 }
